bdb08baa48628d21cc7afb8aeb15be4976667c36
Kevin Läufer <laeufer@cs.berkeley.edu>
2021-07-15 17:38:14 -0700

    multi-clock support is currently disabled

--- src/test/scala/chiseltest/backends/verilator/VerilatorClockPokeTest.scala
+++ src/test/scala/chiseltest/backends/verilator/VerilatorClockPokeTest.scala
@@ -13,7 +13,7 @@ import treadle.{ClockInfoAnnotation, WriteVcdAnnotation}
 class VerilatorClockPokeTest extends AnyFlatSpec with ChiselScalatestTester {
   behavior of "Testers2 with a clock input"
 
-  it should "verilator-clock-poke" in {
+  it should "verilator-clock-poke" ignore { // TODO: re-add multi clock support
     test(new Module {
       val inClock = IO(Input(Clock()))
       val out = IO(Output(UInt(8.W)))
@@ -50,7 +50,7 @@ class VerilatorClockPokeTest extends AnyFlatSpec with ChiselScalatestTester {
     }
   }
 
-  it should "clock-as-bool-verilator-clock-poke" in {
+  it should "clock-as-bool-verilator-clock-poke" ignore { // TODO: re-add multi clock support
     test(new Module {
       val inClock = IO(Input(Bool()))
       val out = IO(Output(UInt(8.W)))
@@ -75,7 +75,7 @@ class VerilatorClockPokeTest extends AnyFlatSpec with ChiselScalatestTester {
     }
   }
 
-  it should "treadle-clock-poke" in {
+  it should "treadle-clock-poke" ignore { // TODO: re-add multi clock support
     test(new Module {
       val inClock = IO(Input(Clock()))
       val out = IO(Output(UInt(8.W)))
