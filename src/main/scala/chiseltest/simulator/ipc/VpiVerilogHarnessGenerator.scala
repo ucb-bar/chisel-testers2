dbf5516962be918af7b8531d9a7d35f4d74d742c
Kevin Läufer <laeufer@cs.berkeley.edu>
2021-07-20 10:23:12 -0700

    some VCS fixes

--- src/main/scala/chiseltest/simulator/ipc/VpiVerilogHarnessGenerator.scala
+++ src/main/scala/chiseltest/simulator/ipc/VpiVerilogHarnessGenerator.scala
@@ -13,10 +13,12 @@ private[chiseltest] object VpiVerilogHarnessGenerator {
   ): String = {
     val dutName = toplevel.name
 
+    require(toplevel.clocks.length <= 1, "Multi clock circuits are currently not supported!")
+    val clockName = toplevel.clocks.headOption.getOrElse("clock")
+
     val codeBuffer = new StringBuilder
     codeBuffer.append("module test;\n")
-    codeBuffer.append("  reg clock = 1;\n")
-    codeBuffer.append("  reg reset = 1;\n")
+    codeBuffer.append(s"  reg $clockName = 1;\n")
     val delay = if (isGateLevel) "#0.1" else ""
     toplevel.inputs.foreach { case (name, width) =>
       codeBuffer.append(s"  reg[${width - 1}:0] $name = 0;\n")
@@ -29,28 +31,26 @@ private[chiseltest] object VpiVerilogHarnessGenerator {
       codeBuffer.append(s"  assign $delay $name = ${name}_delay;\n")
     }
 
-    codeBuffer.append("  always #`CLOCK_PERIOD clock = ~clock;\n")
+    codeBuffer.append(s"  always #`CLOCK_PERIOD $clockName = ~$clockName;\n")
     codeBuffer.append("  reg vcdon = 0;\n")
-    codeBuffer.append("  reg [1023:0] vcdfile = 0;\n")
+    codeBuffer.append("  reg [1023:0] vcdfile = 0;\n") // TODO: why vcd + vpd
     codeBuffer.append("  reg [1023:0] vpdfile = 0;\n")
 
     codeBuffer.append("\n  /*** DUT instantiation ***/\n")
     codeBuffer.append(s"  $dutName $dutName(\n")
-    codeBuffer.append("    .clock(clock),\n")
-    codeBuffer.append("    .reset(reset),\n")
+    codeBuffer.append(toplevel.clocks.map(c => s"    .$c($c),\n"))
     val ioNames = (toplevel.inputs ++ toplevel.outputs).map(_._1)
     codeBuffer.append(ioNames.map(name => s"    .$name(${name}_delay)").mkString(",\n"))
     codeBuffer.append("  );\n\n")
 
     codeBuffer.append("  initial begin\n")
-    codeBuffer.append("    $init_rsts(reset);\n")
     val inputNames = toplevel.inputs.map(_._1)
     codeBuffer.append("    $init_ins(%s);\n".format(inputNames.mkString(", ")))
     val outputNames = toplevel.outputs.map(_._1)
     codeBuffer.append("    $init_outs(%s);\n".format(outputNames.mkString(", ")))
     codeBuffer.append("    $init_sigs(%s);\n".format(dutName))
     codeBuffer.append("    /*** VCD & VPD dump ***/\n")
-    codeBuffer.append("    if ($value$plusargs(\"vcdfile=%s\", vcdfile)) begin\n")
+    codeBuffer.append("    if ($value$plusargs(\"vcdfile=%s\", vcdfile)) begin\n") // TODO: have to pass these flags ourself?
     codeBuffer.append("      $dumpfile(vcdfile);\n")
     codeBuffer.append("      $dumpvars(0, %s);\n".format(dutName))
     codeBuffer.append("      $dumpoff;\n")
@@ -67,12 +67,8 @@ private[chiseltest] object VpiVerilogHarnessGenerator {
     codeBuffer.append("    $vcdpluson(0);\n")
     codeBuffer.append("  end\n\n")
 
-    codeBuffer.append("  always @(%s clock) begin\n".format(if (isGateLevel) "posedge" else "negedge"))
-    codeBuffer.append("    if (vcdfile && reset) begin\n")
-    codeBuffer.append("      $dumpoff;\n")
-    codeBuffer.append("      vcdon = 0;\n")
-    codeBuffer.append("    end\n")
-    codeBuffer.append("    else if (vcdfile && !vcdon) begin\n")
+    codeBuffer.append(s"  always @(%s $clockName)  begin\n".format(if (isGateLevel) "posedge" else "negedge"))
+    codeBuffer.append("    if (vcdfile && !vcdon) begin\n")
     codeBuffer.append("      $dumpon;\n")
     codeBuffer.append("      vcdon = 1;\n")
     codeBuffer.append("    end\n")
